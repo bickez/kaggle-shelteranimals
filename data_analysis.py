1bd9679793d02c6ed5f3cfe5f8e871d1885fcdf9
Zach Bickel <zachbickel91@gmail.com>
2016-07-05 22:46:44 -0700

    First commit

---
+++ data_analysis.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+from collections import defaultdict
+import re
+
+df = pd.read_csv('train.csv')
+
+df['OutcomeType'].value_counts()
+df['AnimalType'].value_counts()
+df[['OutcomeType', 'AnimalType']].describe()
+
+def age_to_months(x):
+	try: 
+		spl = x.split(' ')
+	except AttributeError:
+		return np.nan
+	if spl[1] == 'years' or spl[1] == 'year':
+		return int(spl[0]) * 12
+	else:
+		return int(spl[0])	
+
+df['AgeMonths'] = df['AgeuponOutcome'].apply(lambda x : age_to_months(x))
+df[['AgeMonths', 'AnimalType']].groupby('AnimalType').mean()
+mean_lifespan = df[['AgeMonths', 'AnimalType']].groupby('AnimalType').mean()
+df['RelativeAge'] = np.where(df['AnimalType'] == 'Cat', df['AgeMonths'] / mean_lifespan.loc['Cat'].values, df['AgeMonths'] / mean_lifespan.loc['Dog'].values)
+
+df['has_name'] = pd.isnull(df['Name'])
+
+# figure out color frequencies of various basic colors
+temp = df['Color'].apply(lambda x : re.split('/| Tabby| ', x))
+temp_flat = np.hstack(temp.values)
+un = np.unique(temp_flat)[1:]  # get rid of ''
+# count different colors
+d = defaultdict(int)
+for w in temp_flat:
+	d[w] += 1
+print d
+print un # unique colors
+
+# order of colors with counts > 1000
+l = ['White', 'Black', 'Brown', 'Tan', 'Blue', 'Brindle']
+res_color = []
+for c in df['Color']:
+	ctemp = re.split('/| Tabby| ', c)
+	loc = []
+	for ct in ctemp:
+		if ct in l:
+			loc.append(l.index(ct))
+	if len(loc) > 0:
+		res_color.append(l[min(loc)])
+	else:
+		res_color.append('Other')
+
+df['CleanColor'] = res_color
+print df.head()
+df[['OutcomeType', 'AnimalType', 'has_name', 'RelativeAge', 'CleanColor']].to_csv('train_clean.csv')
+
+
+
---
+++ exploratory_model.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn import preprocessing
+df = pd.read_csv('train_clean.csv')
+df['has_name'] = np.where(df['has_name'], 1, 0)
+df = df.fillna(df.mean())
+print df.head()
+print df.describe()
+print pd.isnull(df)
+le = preprocessing.LabelEncoder()
+target = le.fit_transform(df['OutcomeType'].values)
+features = pd.get_dummies(df[['RelativeAge', 'CleanColor', 'AnimalType', 'has_name']]).values
+#imp = preprocessing.Imputer(missing_values='NaN')  # defaults to using 'mean'
+#imp.fit(features)  # fit the imputer to the features
+#feat_imp = imp.transform(features)  # transform the NaN values
+X_train, X_test, y_train, y_test = train_test_split(features, target, train_size=0.7)
+clf = RandomForestClassifier(n_estimators=100)
+y_pred = clf.fit(X_train, y_train).predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print np.sum(np.diag(cm))*1. / np.sum(cm, axis=None)
+print cm
+print
+
+clf = AdaBoostClassifier(n_estimators=100)
+y_pred = clf.fit(X_train, y_train).predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print np.sum(np.diag(cm))*1. / np.sum(cm, axis=None)
+print cm
+print
+
+clf = GradientBoostingClassifier()
+y_pred = clf.fit(X_train, y_train).predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print np.sum(np.diag(cm))*1. / np.sum(cm, axis=None)
+print cm
+print
